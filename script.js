6dd9ea5a57cea419b569aca63f398d6123fe46e3
Ana M <ana.alyse13@gmail.com>
2018-08-24 10:14:57 -0500

    guess randomNumber and count attempts

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-function randomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
+var upper = 10000;                                /* define upper limit of random number */
+var randomNumber = getRandomNumber(upper);        /* generate the number the computer has to guess. */
+var guess;                                        /* hold the computer's guess */
+var attempts = 0;                                 /* keep track of how many times it take computer to guess */
+
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1; /* functions are also grouped at top of code; reuseable */
 }
-var counter = 0;
-while ( counter < 1000 ) {
-  var randNum = randomNumber(6);
-  document.write(randNum + ' ');
-  counter += 1;
+
+while ( guess !== randomNumber ) {
+  guess = getRandomNumber( upper );                /* store randomly generated number into variable guess */
+  attempts += 1;
 }
+document.write("<p>The random number was: " + randomNumber + "</p>");
+document.write("<p>It took the computer " + attempts + " attemps to get it right.</p>");
